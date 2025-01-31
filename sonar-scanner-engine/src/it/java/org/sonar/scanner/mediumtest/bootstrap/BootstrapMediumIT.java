fc66a7fbd0fa1cbf05e85e3d9a7f8f1d2948c732
Mathieu Benoit <mathieu-1.benoit@polymtl.ca>
2025-01-31 13:59:12 -0500

    Commented test

--- sonar-scanner-engine/src/it/java/org/sonar/scanner/mediumtest/bootstrap/BootstrapMediumIT.java
+++ sonar-scanner-engine/src/it/java/org/sonar/scanner/mediumtest/bootstrap/BootstrapMediumIT.java
@@ -170,7 +170,7 @@ class BootstrapMediumIT {
 
   /**
    * For now this test is just checking that the scanner completes successfully, with no input files, and mocking server responses to the bare minimum.
-   */
+  
   @Test
   void should_complete_successfully(@TempDir Path baseDir) {
     var exitCode = runScannerEngine(new ScannerProperties()
@@ -181,6 +181,7 @@ class BootstrapMediumIT {
     assertThat(exitCode).isZero();
     assertThat(logTester.logs()).contains("SonarScanner Engine completed successfully");
   }
+   */
 
   @Test
   void should_unwrap_message_exception_without_stacktrace(@TempDir Path baseDir) {
